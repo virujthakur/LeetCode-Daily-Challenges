29d077e90e6fb06a5e31b56484cfcf910a00b17e
Viruj Thakur <77432903+VJ1122001@users.noreply.github.com>
2023-12-13 16:22:17 +0530

    Create 13_Dec_2023_1582_Special_Positions_In_A_Binary_Matrix.py

---
+++ 13_Dec_2023_1582_Special_Positions_In_A_Binary_Matrix.py
@@ -0,0 +1,25 @@
+class Solution:
+    # TC: O(N*M (N+M)) SC: O(1)
+    def numSpecial(self, mat: List[List[int]]) -> int:
+        m= len(mat)
+        n= len(mat[0])
+        ans = 0
+        
+        for i in range (m):
+            for j in range (n):
+                if mat[i][j] == 1:
+                    flag= False
+                    for k in range(m):
+                        if i!=k and mat[k][j]==1 :
+                            flag= True
+                        
+                    for k in range(n):
+                        if j!=k and mat[i][k]==1 :
+                            flag= True
+                        
+                    if not flag :
+                        ans+=1
+        return ans
+                    
+                        
+        

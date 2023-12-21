d3823fc5098e16e8f7d8281faf72df16fb544e19
Viruj Thakur <77432903+VJ1122001@users.noreply.github.com>
2023-12-21 07:13:40 +0530

    Solution to the challenge of 21 Dec 2023 Widest vertical area between two points containing no points

---
+++ 21_Dec_2023_1637_Widest_Vertical_Area_Between_Points_Containing_No_Points.py
@@ -0,0 +1,10 @@
+class Solution:
+    #TC: O(N) SC: O(1)
+    def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
+        points.sort()
+        n= len(points)
+        ans= 0
+        for i in range (1,n):
+            ans= max(ans, points[i][0]- points[i-1][0])
+            
+        return ans

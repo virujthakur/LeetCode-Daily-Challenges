4b64e7e84ebcf547cffac58c5ac68ebb3d219bcb
Viruj Thakur <77432903+VJ1122001@users.noreply.github.com>
2024-04-04 16:21:28 +0530

    Solution to the challenge of 04 Apr 2024 Maximum nesting depth of the parantheses

---
+++ 04_Apr_2024_1614_Maximum_Nesting_Depth_Of_The_Parantheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    #TC: O(N) SC: O(1)
+    def maxDepth(self, s: str) -> int:
+        d = 0
+        ans = 0
+        for c in s:
+            if c== '(' :
+                d+=1
+            elif c== ')':
+                d-=1
+            
+            ans= max(ans, d)
+            
+        return ans

dcdaae501bd7ad1686f5f12e85b49df918de663b
Viruj Thakur <77432903+virujthakur@users.noreply.github.com>
2024-06-08 19:39:22 +0530

    Solution to the challenge of 08 Jun 2024 Continuous subarray sum

---
+++ 08_Jun_2024_523_Continous_Subarray_Sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    #TC: O(N) SC: O(1)
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        n= len(nums)
+        f= defaultdict(int)
+        f_occ= defaultdict(int)
+        
+        s = 0
+        f[0] =1
+        f_occ[0]= -1
+        for i,num in enumerate(nums):
+            s+= num
+            rem= s%k
+            if rem in f_occ and i- f_occ[rem] >= 2:
+                return True
+           
+            f[rem] +=1
+            if rem not in f_occ:
+                f_occ[rem] = i
+            
+        return False

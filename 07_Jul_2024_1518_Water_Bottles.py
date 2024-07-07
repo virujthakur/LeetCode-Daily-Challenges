dde7e1b9ed6ccb7f14f95b91b514886c4eba49d6
Viruj Thakur <77432903+virujthakur@users.noreply.github.com>
2024-07-07 17:27:24 +0530

    Solution to the challenge of 07 Jul 2024 Water bottles

---
+++ 07_Jul_2024_1518_Water_Bottles.py
@@ -0,0 +1,13 @@
+class Solution:
+    #TC :O(numBottles) SC: O(1)
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        ans = 0
+        empty = 0
+        while numBottles > 0:
+                
+            ans+=numBottles
+            empty+= numBottles
+            numBottles= empty // numExchange
+            empty = empty% numExchange
+            
+        return ans

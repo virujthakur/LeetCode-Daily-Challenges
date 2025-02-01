6f6dbafe0e70fbaabc0f83736146106b618a5555
Viruj Thakur <77432903+virujthakur@users.noreply.github.com>
2025-02-01 14:53:16 +0530

    Solution to the challenge of 01 Feb 2025 Special Array I

---
+++ 01_Feb_2025_3151_Special_Array_I.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    //TC: O(N) SC: O(1)
+    bool isArraySpecial(vector<int>& nums) {
+        int n = nums.size();
+        for (int i=1; i<n; i++)
+        {
+            if (nums[i]%2 == nums[i-1]% 2)
+            return false;
+        }
+        return true;
+    }
+};

fd3e7d56f87abfc39d6937d86eb5951329549e30
Viruj Thakur <77432903+virujthakur@users.noreply.github.com>
2025-02-06 15:25:58 +0530

    Solution to the challenge of 06 Feb 2025 Tuple with same product

---
+++ 06_Feb_2025_1726_Tuple_with_Same_Product.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    //TC: O(N) SC: O(N)
+    int tupleSameProduct(vector<int>& nums) {
+        int n= nums.size();
+        unordered_map<int,int> f;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=i+1; j<n; j++)
+            {
+                if (nums[i] != nums[j])
+                    f[nums[i]* nums[j]] +=1;
+
+            }
+        }
+
+        int ans = 0;
+        for(auto x: f)
+        {
+            if(x.second >= 2)
+            {
+                ans+= (x.second)* (x.second-1) * 4;
+            }
+        }
+
+        return ans;
+        
+    }
+};

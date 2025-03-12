07c13b6424cbeb0290c8d83defc8822da6c174e3
Viruj Thakur <77432903+virujthakur@users.noreply.github.com>
2025-03-13 01:59:09 +0530

    Solution to the challenge of 12 Mar 2025 Maximum count of positive and negative integer

---
+++ 12_Mar_2025_2529_Maximum_Count_of_Positive_Integer_and_Negative_Integer.py
@@ -0,0 +1,10 @@
+class Solution:
+    #TC: O(NLOGN) SC: O(1)
+    def maximumCount(self, nums: List[int]) -> int:
+        n = len(nums)
+        neg = bisect.bisect_left(nums, 0, 0, len(nums))
+        pos = bisect.bisect_right(nums, 0, 0, len(nums))
+        return max(neg, n-pos)
+
+
+        

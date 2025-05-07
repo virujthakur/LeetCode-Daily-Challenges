223569c8a444f80686d52e4d6be0530cd7a1fbf9
Viruj Thakur <77432903+virujthakur@users.noreply.github.com>
2025-05-07 13:18:22 +0530

    Solution to the challenge of 07 May 2025 Find minimum time to reach last room I

---
+++ 07_May_2025_3341_Find_Minimum_Time_to_Reach_Last_Room_I.py
@@ -0,0 +1,30 @@
+class Solution:
+    #TC: O(M*N* LOG(M*N)) SC: O(N*M)
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        di = [(0,1), (1,0), (0,-1), (-1,0)]
+        pq = []
+
+        n, m = len(moveTime), len(moveTime[0])
+        visited = [[False]* m for _ in range(n)]
+
+        heapq.heappush(pq, (0,0,0))
+        while pq:
+            cur = heapq.heappop(pq)
+            if visited[cur[1]][cur[2]]:
+                continue
+
+            visited[cur[1]][cur[2]]= True
+
+            if cur[1] == n-1 and cur[2] == m-1:
+                return cur[0]
+
+            for d in di:
+                newx = cur[1] + d[0]
+                newy = cur[2] + d[1]
+
+                if newx >=0 and newy >=0 and newx<n and newy<m and visited[newx][newy]== False:
+                    heapq.heappush(pq, (max(cur[0], moveTime[newx][newy]) + 1, newx, newy))
+
+            # print(pq)
+
+        return -1
